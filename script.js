80d0c506683b28afe1a51fb73eeaa536dd07c2ca
Resiliance-Chado <theartofeditpro@gmail.com>
2026-04-18 15:43:04 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function calculer() {
-  let distance = document.getElementById("distance").value;
-  let temps = document.getElementById("temps").value;
-
-  let allure = temps / distance;
-
-  document.getElementById("resultat").innerHTML =
-    "Allure : " + allure.toFixed(2) + " min/km";
-}
